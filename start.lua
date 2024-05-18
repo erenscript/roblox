0b6110f94f18eec2c4543c0de54756dfdaf432ac
erenscript <117516146+erenscript@users.noreply.github.com>
2024-05-18 19:47:11 +0300

    Update start.lua

--- start.lua
+++ start.lua
@@ -7,6 +7,7 @@ local ScreenGui = Instance.new("ScreenGui")
 local Frame = Instance.new("Frame")
 local TextButton = Instance.new("TextButton")
 local ImageLabel = Instance.new("ImageLabel")
+local TextButton_2 = Instance.new("TextButton")
 
 --Properties:
 
@@ -17,7 +18,7 @@ Frame.Parent = ScreenGui
 Frame.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
 Frame.BorderColor3 = Color3.fromRGB(0, 0, 0)
 Frame.BorderSizePixel = 0
-Frame.Position = UDim2.new(0.33192718, 0, 0.213884979, 0)
+Frame.Position = UDim2.new(0.402003348, 0, 0.27373454, 0)
 Frame.Size = UDim2.new(0, 333, 0, 326)
 
 TextButton.Parent = Frame
@@ -28,7 +29,7 @@ TextButton.BorderSizePixel = 0
 TextButton.Position = UDim2.new(0.225225225, 0, 0.177914113, 0)
 TextButton.Size = UDim2.new(0, 200, 0, 50)
 TextButton.Font = Enum.Font.SourceSansBold
-TextButton.Text = "Berke Smoke Dedected!!!"
+TextButton.Text = "Olcay Sarı dedected"
 TextButton.TextColor3 = Color3.fromRGB(255, 0, 0)
 TextButton.TextSize = 30.000
 
@@ -38,4 +39,16 @@ ImageLabel.BorderColor3 = Color3.fromRGB(0, 0, 0)
 ImageLabel.BorderSizePixel = 0
 ImageLabel.Position = UDim2.new(0.0660660639, 0, 0.331288338, 0)
 ImageLabel.Size = UDim2.new(0, 288, 0, 206)
-ImageLabel.Image = "http://www.roblox.com/asset/?id=17534149452"
+ImageLabel.Image = "http://www.roblox.com/asset/?id=17534262915"
+
+TextButton_2.Parent = ScreenGui
+TextButton_2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextButton_2.BackgroundTransparency = 1.000
+TextButton_2.BorderColor3 = Color3.fromRGB(0, 0, 0)
+TextButton_2.BorderSizePixel = 0
+TextButton_2.Position = UDim2.new(0.0882789344, 0, 0, 0)
+TextButton_2.Size = UDim2.new(0, 1156, 0, 50)
+TextButton_2.Font = Enum.Font.SourceSansBold
+TextButton_2.Text = "LÜTFEN ACİLEN OYUNU KAPATINIZ YOKSA MUHSİN SARI SİZİ SİKECEK"
+TextButton_2.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextButton_2.TextSize = 30.000
